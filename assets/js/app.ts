7566eeb4f379fe7fe9ab39d43de6ff5054d74c1a
Amaranth01 <vanessa.nectoux@gmail.com>
2022-11-24 11:05:29 +0100

    add webpack

---
+++ assets/js/app.ts
@@ -0,0 +1,8 @@
+let monString: string = '9string';
+let monNombre: number = 546546;
+
+console.log(monString);
+console.log(monNombre);
+
+let result: number = monNombre + parseInt(monString);
+console.log(result);
